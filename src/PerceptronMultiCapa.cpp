039155d550801c636dccfccd7d219fc954c79a97
ggomez1728 <ggomez1728@gmail.com>
2015-08-17 00:00:34 -0430

    Estructura de menu de pruebas basico

--- src/PerceptronMultiCapa.cpp
+++ src/PerceptronMultiCapa.cpp
@@ -19,10 +19,44 @@ int main() {
   vector<Caso> dataTrain;
   srand (time(NULL));
   Data data;
+  string la;
   dataTrain=data.leerData("data_train.txt");
   redMultiLabelPerceptron RNA(data.getCapasRNA(),data.getEntradasRNA(),data.getOcultasRNA(),data.getSalidasRNA());
-  RNA.inicializarPesos();
+inicializar:
+  cout<<"(1) Inicializar Conexiones Aleatoriamente \n(2) Cargar Conexiones Sinápticas \n(x) Salir \n >";
+  cin>>la;
+  if (la=="1"){
+	//inicializacion mediante modelo aleatorio
+	RNA.inicializarPesos();
+	cout<<"Conexiones Inicializadas Aleatoriamente"<<endl;
+  }
+  else if(la =="2"){
+	//inicializacion mediante archivo de los pesos o conexiones
+    cout<<"Conexiones Inicializadas desde Archivo"<<endl;
+  }
+  else if(la =="x"){
+    goto finPrograma;
+  }
+  else goto inicializar;
+Acciones:
+  cout<<"(1) Entrenar Perceptron \n(2) Ejecutar Perceptron \n(3) Meter Ruido \n(x) Salir \n >";
+  cin>>la;
+  if(la=="1"){
+
+  }
+  else if(la=="2"){
+
+  }
+  else if(la=="3"){
+
+  }
+  else if(la=="x"){
+    goto finPrograma;
+  }
+  else goto Acciones;
 
 	cout << "!!!Perceptron MultiCapa!!!" << endl; // prints !!!Hello World!!!
+finPrograma:
+  cout<<"Saliendo...";
 	return 0;
 }
